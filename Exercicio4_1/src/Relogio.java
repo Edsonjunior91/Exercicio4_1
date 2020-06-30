7cbef3b284a5b997e2fa117431459f43ef8210f5
Edson Junior <edson_junior91@hotmail.com>
2020-06-30 00:32:07 -0300

    Treino de chamada de métodos e atributos

---
+++ Exercicio4_1/src/Relogio.java
@@ -0,0 +1,25 @@
+
+public class Relogio {
+	Ponteiro ponteiroHora = new Ponteiro();
+	Ponteiro ponteiroMinuto = new Ponteiro();
+	Ponteiro ponteiroSegundo = new Ponteiro();
+	
+	void acertarRelogio (int hora, int minuto, int segundo) {
+		
+		hora = hora % 12;
+		
+		ponteiroHora.posicao = hora;
+		ponteiroMinuto.posicao = minuto /5;
+		ponteiroSegundo.posicao = segundo /5 ;
+	}
+	
+	int lerHora() {
+		return ponteiroHora.posicao;
+	}
+	int lerMinuto() {
+		return ponteiroMinuto.posicao;
+	}
+	int lerSegundo() {
+		return ponteiroSegundo.posicao;
+	}
+}
